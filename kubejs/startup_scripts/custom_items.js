aaaf34d8b5936173138ef596294b87fe3de7593a
Rosie-Holstein <creeper620@gmail.com>
2024-04-01 21:57:05 -0400

    Removed string mesh item and texture
    No longer used in glider recipe, so was removed

--- kubejs/startup_scripts/custom_items.js
+++ kubejs/startup_scripts/custom_items.js
@@ -1,6 +1,5 @@
 // Listen to item registry event
 StartupEvents.registry('item', e => {
   e.create('inert_totem').maxStackSize(1)
-  e.create('string_mesh')
   e.create('arrow_bundle').maxStackSize(16)
 })
