7e4463bde37de1ef689f3d48bccf6da517722831
Rosie-Holstein <creeper620@gmail.com>
2024-05-12 17:39:45 -0400

    Update custom_items.js
    Add hammer smithing template custom item

--- kubejs/startup_scripts/custom_items.js
+++ kubejs/startup_scripts/custom_items.js
@@ -1,5 +1,6 @@
 // Listen to item registry event
 StartupEvents.registry('item', e => {
   e.create('inert_totem').maxStackSize(1)
+  e.create('hammer_smithing_template')
   e.create('arrow_bundle').maxStackSize(16)
 })
